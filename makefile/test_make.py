a32af066afe8b5539498f383b5f0ff3ecb459616
ponty <ponty@home>
2014-03-27 18:36:44 +0100

    fix build test

--- makefile/test_make.py
+++ makefile/test_make.py
@@ -1,15 +1,15 @@
 from easyprocess import EasyProcess
 from nose.tools import eq_
-from util import tmpdir
 import os
-import shutil
+import util
 
-
-FIRMWARE = os.path.dirname(__file__) + '/../firmware/Nanpy'
+FIRMWARE = os.path.dirname(__file__) + '/../Nanpy'
+MAKEFILE_AND_LIB = os.path.dirname(__file__) + '/makefile_stuff'
 
 
 def test_make():
-    dest = tmpdir() + '/Nanpy'
-    shutil.copytree(FIRMWARE, dest)
+    dest = util.tmpdir() + '/Nanpy'
+    util.copytree(FIRMWARE, dest)
+    util.copytree(MAKEFILE_AND_LIB, dest)
     p = EasyProcess('make size BOARD=uno', cwd=dest).call()
     eq_(p.return_code, 0)
